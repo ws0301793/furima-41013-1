fe130a7ab3c2ebdb9343d5f271f270d27bacfcd9
ws0301793 <wtr290503@gmail.com>
2024-09-30 20:40:57 +0900

    shippingモデル作成～マイグレーション実行

--- db/migrate/20240930112738_create_shippings.rb
+++ db/migrate/20240930112738_create_shippings.rb
@@ -1,7 +1,13 @@
 class CreateShippings < ActiveRecord::Migration[7.0]
   def change
     create_table :shippings do |t|
-
+      t.string     :postcord,           null: false
+      t.integer    :prefecture_id,      null: false
+      t.string     :municipalities,     null: false
+      t.string     :street,             null: false
+      t.string     :building,           null: false
+      t.string     :telephone_number,   null: false
+      t.references :order,              null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_29_085528) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_30_112738) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_29_085528) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "shippings", charset: "utf8", force: :cascade do |t|
+    t.string "postcord", null: false
+    t.integer "prefecture_id", null: false
+    t.string "municipalities", null: false
+    t.string "street", null: false
+    t.string "building", null: false
+    t.string "telephone_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shippings_on_order_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -86,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_29_085528) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "shippings", "orders"
 end
