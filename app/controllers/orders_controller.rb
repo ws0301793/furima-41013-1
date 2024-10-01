efe2831a02e21b8b029aa45700abeafad3718d59
ws0301793 <wtr290503@gmail.com>
2024-10-01 20:35:10 +0900

    バリデーションを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.create(order_params)
+    Shipping.create(shipping_params)
+    redirect_to root_path
   end
-  
+
+  private
+
+  def order_params
+    params.merge(user_id: current_user.id)
+  end
+
+  def shipping_params
+    params.permit(:postcord, :prefecture_id, :municipalities, :street, :building, :telephone_number).merge(order_id: @order.id)
+  end
+
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,4 +1,15 @@
 class OrderShipping
   include ActiveModel::Model
   attr_accessor :postcord, :prefecture_id, :municipalities, :street, :building, :telephone_number, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :telephone_number, format: {with:/\A\d{10,11}\z/, message: "can't be blank"}
+    validates :item_id
+    validates :user_id
+    validates :postcord, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipalities, {message: "can't be blank"}
+    validates :street, {message: "can't be blank"}
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
 end
